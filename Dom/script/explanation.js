49d80f1e998a02c661f0fe16dcbb257384228c9d
Manjioko <643898155@qq.com>
2017-09-20 21:25:49 +0800

    explanation have been added some new feature.

--- Dom/script/explanation.js
+++ Dom/script/explanation.js
@@ -6,9 +6,30 @@ function displayAbbreviations(){
         var definition=abbreviations[i].getAttribute("title");
         var key=abbreviations[i].lastChild.nodeValue;
         defs[key]=definition;
-   //创建标记
     }
     alert("working!");
+
+   //创建标记
+    var dlist=document.createElement("dl");
+    for (key in defs){
+        var definition=defs[key];
+        var dtitle=document.createElement("dt");
+        var dtitle_text=document.createTextNode(key+":");
+        dtitle.appendChild(dtitle_text);
+        var ddesc=document.createElement("dd");
+        var ddesc_text=document.createTextNode(definition);
+        ddesc.appendChild(ddesc_text);
+           
+        dlist.appendChild(dtitle);
+
+        dlist.appendChild(ddesc);
+    }
+    var header=document.createElement("h2");
+    var header_text=document.createTextNode("Abbreviation");
+    header.appendChild(header_text);
+    document.body.appendChild(header);
+    document.body.appendChild(dlist);
+
 }
 
 addLoadEvent(displayAbbreviations);
